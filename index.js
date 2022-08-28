f770d206ad3dd2ad76926da5487b7ed632145b40
Tala-coder <muhammedtalic.it@gmail.com>
2022-08-29 00:17:58 +0200

    .

--- index.js
+++ index.js
@@ -7,37 +7,41 @@ import cors from 'cors';
 const server = http.createServer(app);
 
 app.use(cors());
-// app.use(express.json());
+app.use(express.json());
 
-// app.get('/test', (req, res) => {
-//     res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
-// })
+app.get('/', (req, res) => {
+    res.send({body: req.body});
+})
 
-const io = new Server(server, {
-    cors: {
-        origin: "*"
-    },
-});
+app.get('/test', (req, res) => {
+    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
+})
 
-let rijec = 'react'
-let brojac = 0;
-io.on("connection", (socket) => {
-    socket.on("join_room", (data) => {
-        console.log('user join in Room', data)
-        brojac++;
-        brojac > 1 ? io.emit("noviIgrac", { rijec }) : undefined;
-    });
+// const io = new Server(server, {
+//     cors: {
+//         origin: "*"
+//     },
+// });
 
-    socket.once("send_message", (data) => {
-        // io.to(data.room).emit("receive_message", data);
-        console.log('primio poruku na bekendu')
-        io.emit("receive_message", data);
-    });
+// let rijec = 'react'
+// let brojac = 0;
+// io.on("connection", (socket) => {
+//     socket.on("join_room", (data) => {
+//         console.log('user join in Room', data)
+//         brojac++;
+//         brojac > 1 ? io.emit("noviIgrac", { rijec }) : undefined;
+//     });
 
-    socket.on("kraj_igre", () => {
-        brojac = 0;
-    })
-});
+//     socket.once("send_message", (data) => {
+//         // io.to(data.room).emit("receive_message", data);
+//         console.log('primio poruku na bekendu')
+//         io.emit("receive_message", data);
+//     });
+
+//     socket.on("kraj_igre", () => {
+//         brojac = 0;
+//     })
+// });
 
 const PORT = process.env.PORT || '5000';
 server.listen(PORT, () => {
