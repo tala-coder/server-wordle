8a3e051ed70247ba5e9dbed67cdb927917c2782f
Tala-coder <muhammedtalic.it@gmail.com>
2022-08-28 20:49:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const app = express();
 import http from 'http';
-// import { Server } from 'socket.io';
+import { Server } from 'socket.io';
 import cors from 'cors';
 
 const server = http.createServer(app);
@@ -9,39 +9,39 @@ const server = http.createServer(app);
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
     res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
 })
 
-// const io = new Server(server, {
-//     cors: {
-//         origin: "*"
-//     },
-// });
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    },
+});
 
-// app.get('/test', (req, res) => {
-//     res.send('welcome test')
-// })
+app.get('/test', (req, res) => {
+    res.send('welcome test')
+})
 
-// let rijec = 'react'
-// let brojac = 0;
-// io.on("connection", (socket) => {
-//     socket.on("join_room", (data) => {
-//         console.log('user join in Room', data)
-//         brojac++;
-//         brojac > 1 ? io.emit("noviIgrac", { rijec }) : undefined;
-//     });
+let rijec = 'react'
+let brojac = 0;
+io.on("connection", (socket) => {
+    socket.on("join_room", (data) => {
+        console.log('user join in Room', data)
+        brojac++;
+        brojac > 1 ? io.emit("noviIgrac", { rijec }) : undefined;
+    });
 
-//     socket.once("send_message", (data) => {
-//         // io.to(data.room).emit("receive_message", data);
-//         console.log('primio poruku na bekendu')
-//         io.emit("receive_message", data);
-//     });
+    socket.once("send_message", (data) => {
+        // io.to(data.room).emit("receive_message", data);
+        console.log('primio poruku na bekendu')
+        io.emit("receive_message", data);
+    });
 
-//     socket.on("kraj_igre", () => {
-//         brojac = 0;
-//     })
-// });
+    socket.on("kraj_igre", () => {
+        brojac = 0;
+    })
+});
 
 const PORT = process.env.PORT || '5000';
 server.listen(PORT, () => {
