0afc49f2d1fb183474ab81f5f3276edc5111ae4e
Tala-coder <muhammedtalic.it@gmail.com>
2022-08-28 18:59:17 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+const app = express();
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+const server = http.createServer(app);
+
+app.use(cors());
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    },
+});
+
+let rijec = 'react'
+let brojac = 0;
+io.on("connection", (socket) => {
+    socket.on("join_room", (data) => {
+        console.log('user join in Room', data)
+        brojac++;
+        brojac > 1 ? io.emit("noviIgrac", { rijec }) : undefined;
+    });
+
+    socket.once("send_message", (data) => {
+        // io.to(data.room).emit("receive_message", data);
+        console.log('primio poruku na bekendu')
+        io.emit("receive_message", data);
+    });
+
+    socket.on("kraj_igre", () => {
+        brojac = 0;
+    })
+});
+
+server.listen(3001, () => {
+    console.log("SERVER IS RUNNING");
+});
